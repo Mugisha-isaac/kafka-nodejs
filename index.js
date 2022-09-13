ae8f22d5e76c29114276fa29a8ca880384405905
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-13 11:04:19 +0200

    [feat: index] added a consumer

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ const main = async () => {
   });
 
   await producer.disconnect();
-};
+  const consumer = kafka.consumer({ groupId: "test-group" });
 
+  await consumer.connect();
+  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+};
 
 main();
